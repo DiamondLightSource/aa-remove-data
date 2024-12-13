8c179653c21b54d33c11b57d7db50aa7dd6aa892
Jacob Williamson <hvj62882@pc0132.cs.diamond.ac.uk>
2024-12-13 13:28:10 +0000

    Chunk data when read/writing

--- src/aa_remove_data/pb_utils.py
+++ src/aa_remove_data/pb_utils.py
@@ -1,24 +1,40 @@
 import argparse
 from collections.abc import Callable
 from datetime import datetime, timedelta
+from itertools import islice
 from os import PathLike
+<<<<<<< HEAD
 from pathlib import Path
+=======
+import subprocess
+>>>>>>> 62ba542 (Chunk data when read/writing)
 
 from aa_remove_data.generated import EPICSEvent_pb2
 
 
 class PBUtils:
-    def __init__(self, filepath: PathLike | None = None):
+    def __init__(self, filepath: PathLike | None = None, chunk_size=10000000):
         """Initialise a PBUtils object. If filepath is set, read the protobuf
         file at this location to gether its header, samples and type.
 
         Args:
             filepath (Optional[PathLike], optional): Path to PB file to be
             read. Defaults to None.
+            chunk_size (Optional[int], optional): Number of lines to read/write
+            at one time.
         """
         self.header = EPICSEvent_pb2.PayloadInfo()  # type: ignore
         self.samples = []
+<<<<<<< HEAD
         self.pv_type = ""
+=======
+        self.sample_type = ""
+        self.chunked = False
+        self.read_done = False
+        self._chunk_size = chunk_size
+        self._start_line = 0
+        self._write_started = False
+>>>>>>> 62ba542 (Chunk data when read/writing)
         if filepath:
             self.read_pb(filepath)
 
@@ -106,12 +122,36 @@ class PBUtils:
         Returns:
             Callable: EPICSEvent_pb2 protocol buffer class.
         """
+<<<<<<< HEAD
         # Ensure self.pv_type is set first.
         if not self.pv_type:
             self.pv_type = self.get_pv_type()
         pv_type_camel = self._get_proto_class_name()
         proto_class = getattr(EPICSEvent_pb2, pv_type_camel)
         return proto_class
+=======
+        # Ensure self.sample_type is set first.
+        if not self.sample_type:
+            self.sample_type = self.get_sample_type()
+        sample_type_camel = self._convert_to_class_name(self.sample_type)
+        sample_class = getattr(EPICSEvent_pb2, sample_type_camel)
+        return sample_class
+
+    def generate_test_samples(self, sample_type=6, lines=100, year=2024, 
+                              start=0, seconds_gap=1, nano_gap=0):
+        self.header.pvname = 'test'
+        self.header.year = year
+        self.header.type = sample_type
+        sample_class = self.get_sample_class()
+        self.samples = [sample_class() for n in range(lines)]
+        time_gap = seconds_gap * 10**9 + nano_gap
+        time = start * 10**9
+        for i, sample in enumerate(self.samples):
+            sample.secondsintoyear = time // 10**9
+            sample.nano = time % 10 ** 9
+            sample.val = i
+            time += time_gap
+>>>>>>> 62ba542 (Chunk data when read/writing)
 
     def write_to_txt(self, filepath: PathLike):
         """Write a text file from a PBUtils object.
@@ -139,6 +179,7 @@ class PBUtils:
             filepath (PathLike): Path to PB file.
         """
         with open(filepath, "rb") as f:
+<<<<<<< HEAD
             first_line = self._restore_newline_chars(f.readline().strip())
             lines = f.readlines()
         self.header.ParseFromString(first_line)
@@ -147,6 +188,30 @@ class PBUtils:
         for i, sample in enumerate(self.samples):
             line = self._restore_newline_chars(lines[i].strip())
             sample.ParseFromString(line)
+=======
+            if self._start_line == 0:
+                result = subprocess.run(['wc', '-l', filepath],
+                                        stdout=subprocess.PIPE, text=True)
+                self._total_lines = int(result.stdout.split()[0])
+                print(self._total_lines)
+                first_line = self._unescape_data(f.readline().strip())
+                self.header.ParseFromString(first_line)
+                f.seek(0)
+                self._start_line += 1
+            end_line = min(self._start_line + self._chunk_size,
+                           self._total_lines)
+            lines = list(islice(f, self._start_line, end_line))
+            if self._total_lines == end_line:
+                self.read_done = True
+            else:
+                self.chunked = True
+            self._start_line = end_line
+            sample_class = self.get_sample_class()
+            self.samples = [sample_class() for n in range(len(lines))]
+            for i, sample in enumerate(self.samples):
+                line = self._unescape_data(lines[i].strip())
+                sample.ParseFromString(line)
+>>>>>>> 62ba542 (Chunk data when read/writing)
 
     def write_pb(self, filepath: PathLike):
         """Write a PB file that is structured in the Archiver Appliance format.
@@ -155,11 +220,15 @@ class PBUtils:
         Args:
             filepath (PathLike): Path to file to be written.
         """
+<<<<<<< HEAD
         header_b = self._replace_newline_chars(self.header.SerializeToString()) + b"\n"
+=======
+>>>>>>> 62ba542 (Chunk data when read/writing)
         samples_b = [
             self._replace_newline_chars(sample.SerializeToString()) + b"\n"
             for sample in self.samples
         ]
+<<<<<<< HEAD
         with open(filepath, "wb") as f:
             f.writelines([header_b] + samples_b)
 
@@ -201,3 +270,14 @@ def print_header():
         print(f"DATE{' ' * 19}SECONDS{' ' * 5}NANO{' ' * 9}VAL")
         for i in range(lines):
             print(pb.format_datastr(pb.samples[i], year).strip())
+=======
+        if self._write_started is False:    # Write header, start new file
+            header_b = self._escape_data(self.header.SerializeToString()) \
+                + b"\n"
+            with open(filepath, "wb") as f:
+                f.writelines([header_b] + samples_b)
+            self._write_started = True
+        else:                               # Add to existing file
+            with open(filepath, "ab") as f:
+                f.writelines(samples_b)
+>>>>>>> 62ba542 (Chunk data when read/writing)
