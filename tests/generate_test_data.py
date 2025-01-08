68ecfcc4d7bf005380d9977b74af55d9dc3fe401
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2025-01-08 10:28:03 +0000

    Fix bug

--- tests/generate_test_data.py
+++ tests/generate_test_data.py
@@ -9,7 +9,6 @@ def generate_test_data():
     pb = PBUtils()
     for i in (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14):
         pb.generate_test_samples(pv_type=i, start=i * 100)
-        pb._write_started = False
         pb.write_to_txt(Path(f"tests/test_data/{pb.pv_type}_test_data.txt"))
         pb.write_pb(Path(f"tests/test_data/{pb.pv_type}_test_data.pb"))
 
