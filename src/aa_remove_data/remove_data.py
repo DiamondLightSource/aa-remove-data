8e23fa29f2c61eb2d6368e8052f210da2c16533c
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2025-01-14 17:11:00 +0000

    Tweak entry points

--- src/aa_remove_data/remove_data.py
+++ src/aa_remove_data/remove_data.py
@@ -344,6 +344,7 @@ def aa_remove_data_before():
     if len(timestamp) == 1:
         timestamp.append(1)
 
+    txt_filepath = new_pb.with_suffix(".txt")
     diff = datetime(*([year] + timestamp)) - datetime(year, 1, 1)
     seconds = int(diff.total_seconds())
     txt_filepath = new_pb.with_suffix(".txt")
@@ -394,6 +395,7 @@ def aa_remove_data_after():
     if len(timestamp) == 1:
         timestamp.append(1)
 
+    txt_filepath = new_pb.with_suffix(".txt")
     diff = datetime(*([year] + timestamp)) - datetime(year, 1, 1)
     seconds = int(diff.total_seconds())
     txt_filepath = new_pb.with_suffix(".txt")
