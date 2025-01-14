661f83a8755d6e660f4414d1fb3f44eacb8bb0a9
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2025-01-14 16:28:19 +0000

    Test CLI

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,10 +1,15 @@
 import filecmp
 import subprocess
 import sys
+from os import PathLike
 from pathlib import Path
 
 from aa_remove_data import __version__
 from aa_remove_data.pb_utils import PBUtils
+
+test_data = Path("tests/test_data")
+cli_output = Path("tests/test_data/cli_expected_output")
+results = Path("tests/test_data/results_files")
 
 
 def test_cli_version():
@@ -13,13 +18,13 @@ def test_cli_version():
 
 
 def test_cli_print_header():
-    cmd = ["aa-print-header", "tests/test_data/P:2021_short.pb"]
+    cmd = ["aa-print-header", test_data / "P:2021_short.pb"]
     expected = "Name: BL13I-VA-GAUGE-28:P, Type: SCALAR_DOUBLE, Year: 2021"
     assert subprocess.check_output(cmd).decode().strip() == expected
 
 
 def test_cli_print_header_with_lines():
-    cmd = ["aa-print-header", "tests/test_data/RAW:2025_short.pb", "--lines=3"]
+    cmd = ["aa-print-header", test_data / "RAW:2025_short.pb", "--lines=3"]
     expected = (
         "Name: BL11K-EA-ADC-01:M4:CH4:RAW, Type: SCALAR_INT, Year: 2025\n"
         + "DATE                   SECONDS     NANO         VAL\n"
@@ -31,9 +36,9 @@ def test_cli_print_header_with_lines():
 
 
 def test_cli_pb_2_txt():
-    read = "tests/test_data/RAW:2025_short.pb"
-    write = "tests/test_data/RAW:2025_short_test_cli_pb_2_txt.txt"
-    expected = "tests/test_data/RAW:2025_short.txt"
+    read = test_data / "RAW:2025_short.pb"
+    write = results / "RAW:2025_short_test_cli_pb_2_txt.txt"
+    expected = test_data / "RAW:2025_short.txt"
     cmd = ["pb-2-txt", read, write]
     subprocess.run(cmd)
     are_identical = filecmp.cmp(write, expected, shallow=False)
@@ -43,10 +48,30 @@ def test_cli_pb_2_txt():
         write.unlink()
 
 
-def test_cli_reduce_data_freq():
-    read = "tests/test_data/RAW:2025_short.pb"
-    write = "tests/test_data/RAW:2025_test_reduce_data_freq.pb"
-    backup = "tests/test_data/tmp.pb"
+def test_cli_reduce_freq():
+    read = test_data / "SCALAR_STRING_test_data.pb"
+    write = results / "SCALAR_STRING_reduce_freq.pb"
+    backup = results / "tmp.pb"
+    expected = cli_output / "SCALAR_STRING_reduce_freq.pb"
+    period = "4.5"
+    cmd = [
+        "aa-reduce-data-freq",
+        read,
+        period,
+        f"--new-filename={write}",
+        f"--backup-filename={backup}",
+    ]
+    subprocess.run(cmd)
+    try_to_remove(backup)
+    are_identical = filecmp.cmp(write, expected, shallow=False)
+    assert are_identical
+    write.unlink()
+
+
+def test_cli_reduce_data_freq_check_period():
+    read = test_data / "RAW:2025_short.pb"
+    write = results / "RAW:2025_test_reduce_data_freq.pb"
+    backup = results / "tmp.pb"
     period = 3
     cmd = [
         "aa-reduce-data-freq",
@@ -56,43 +81,63 @@ def test_cli_reduce_data_freq():
         f"--backup-filename={backup}",
     ]
     subprocess.run(cmd)
-    backup = Path(backup)
-    backup.unlink()
-    write = Path(write)
+    try_to_remove(backup)
     pb = PBUtils(write)
     for i in range(len(pb.samples) - 1):
         seconds_diff = pb.samples[i + 1].secondsintoyear - pb.samples[i].secondsintoyear
         nano_diff = pb.samples[i + 1].nano - pb.samples[i].nano
-        assert seconds_diff >= 4 or (seconds_diff == 3 and nano_diff >= 0)
+        assert seconds_diff >= period + 1 or (seconds_diff == period and nano_diff >= 0)
     write.unlink()
 
 
 def test_cli_reduce_data_freq_backup():
-    read = "tests/test_data/RAW:2025_short.pb"
-    write = "tests/test_data/tmp.pb"
-    backup = "tests/test_data/RAW:2025_short_backup.pb"
-    period = 3
+    read = test_data / "RAW:2025_short.pb"
+    write = results / "tmp.pb"
+    backup = results / "RAW:2025_short_reduce_data_freq_backup.pb"
+    expected = read
+    period = "3"
     cmd = [
         "aa-reduce-data-freq",
         read,
-        str(period),
+        period,
         f"--new-filename={write}",
         f"--backup-filename={backup}",
     ]
     subprocess.run(cmd)
-    write = Path(write)
-    write.unlink()
-    backup = Path(backup)
-    are_identical = filecmp.cmp(read, backup, shallow=False)
+    try_to_remove(write)
+    are_identical = filecmp.cmp(backup, expected, shallow=False)
     assert are_identical
     backup.unlink()
 
 
+def test_cli_reduce_data_freq_txt():
+    read = test_data / "SCALAR_STRING_test_data.pb"
+    write = results / "SCALAR_STRING_reduce_freq_txt.pb"
+    backup = results / "tmp.pb"
+    expected = cli_output / "SCALAR_STRING_reduce_freq.txt"
+    txt_path = write.with_suffix(".txt")
+    period = "4.5"
+    cmd = [
+        "aa-reduce-data-freq",
+        read,
+        period,
+        "-t",
+        f"--new-filename={write}",
+        f"--backup-filename={backup}",
+    ]
+    subprocess.run(cmd)
+    try_to_remove(write)
+    try_to_remove(backup)
+    are_identical = filecmp.cmp(txt_path, expected, shallow=False)
+    assert are_identical
+    txt_path.unlink()
+
+
 def test_cli_reduce_by_factor():
-    read = "tests/test_data/SCALAR_STRING_test_data.pb"
-    write = "tests/test_data/results/SCALAR_STRING_test_reduce_by_factor.pb"
-    backup = "tests/test_data/results/tmp.pb"
-    expected = Path("tests/test_data/cli_expected_output/SCALAR_STRING_by_factor.pb")
+    read = test_data / "SCALAR_STRING_test_data.pb"
+    write = results / "SCALAR_STRING_reduce_by_factor.pb"
+    backup = results / "tmp.pb"
+    expected = cli_output / "SCALAR_STRING_reduce_by_factor.pb"
     factor = "3"
     cmd = [
         "aa-reduce-data-by-factor",
@@ -102,9 +147,245 @@ def test_cli_reduce_by_factor():
         f"--backup-filename={backup}",
     ]
     subprocess.run(cmd)
-    backup = Path(backup)
-    backup.unlink()
-    write = Path(write)
+    try_to_remove(backup)
     are_identical = filecmp.cmp(write, expected, shallow=False)
     assert are_identical
     write.unlink()
+
+
+def test_cli_reduce_by_factor_backup():
+    read = test_data / "SCALAR_STRING_test_data.pb"
+    write = results / "tmp.pb"
+    backup = results / "SCALAR_STRING_reduce_by_factor_backup.pb"
+    expected = read
+    factor = "3"
+    cmd = [
+        "aa-reduce-data-by-factor",
+        read,
+        factor,
+        f"--new-filename={write}",
+        f"--backup-filename={backup}",
+    ]
+    subprocess.run(cmd)
+    try_to_remove(write)
+    are_identical = filecmp.cmp(backup, expected, shallow=False)
+    assert are_identical
+    backup.unlink()
+
+
+def test_cli_reduce_by_factor_txt():
+    read = test_data / "SCALAR_STRING_test_data.pb"
+    write = results / "SCALAR_STRING_reduce_by_factor_txt.pb"
+    backup = results / "tmp.pb"
+    expected = cli_output / "SCALAR_STRING_reduce_by_factor.txt"
+    txt_path = write.with_suffix(".txt")
+    factor = "3"
+    cmd = [
+        "aa-reduce-data-by-factor",
+        read,
+        factor,
+        "-t",
+        f"--new-filename={write}",
+        f"--backup-filename={backup}",
+    ]
+    subprocess.run(cmd)
+    try_to_remove(write)
+    try_to_remove(backup)
+    are_identical = filecmp.cmp(txt_path, expected, shallow=False)
+    assert are_identical
+    txt_path.unlink()
+
+
+def test_cli_remove_every_nth():
+    read = test_data / "SCALAR_STRING_test_data.pb"
+    write = results / "SCALAR_STRING_reduce_by_factor.pb"
+    backup = results / "tmp.pb"
+    expected = cli_output / "SCALAR_STRING_remove_every_nth.pb"
+    factor = "3"
+    cmd = [
+        "aa-remove-data-every-nth",
+        read,
+        factor,
+        f"--new-filename={write}",
+        f"--backup-filename={backup}",
+    ]
+    subprocess.run(cmd)
+    try_to_remove(backup)
+    are_identical = filecmp.cmp(write, expected, shallow=False)
+    assert are_identical
+    write.unlink()
+
+
+def test_cli_remove_every_nth_backup():
+    read = test_data / "SCALAR_STRING_test_data.pb"
+    write = results / "tmp.pb"
+    backup = results / "SCALAR_STRING_remove_every_nth_backup.pb"
+    expected = read
+    factor = "3"
+    cmd = [
+        "aa-remove-data-every-nth",
+        read,
+        factor,
+        f"--new-filename={write}",
+        f"--backup-filename={backup}",
+    ]
+    subprocess.run(cmd)
+    try_to_remove(write)
+    are_identical = filecmp.cmp(backup, expected, shallow=False)
+    assert are_identical
+    backup.unlink()
+
+
+def test_cli_remove_every_nth_txt():
+    read = test_data / "SCALAR_STRING_test_data.pb"
+    write = results / "SCALAR_STRING_remove_every_nth_txt.pb"
+    backup = results / "tmp.pb"
+    expected = cli_output / "SCALAR_STRING_remove_every_nth.txt"
+    txt_path = write.with_suffix(".txt")
+    factor = "3"
+    cmd = [
+        "aa-remove-data-every-nth",
+        read,
+        factor,
+        "-t",
+        f"--new-filename={write}",
+        f"--backup-filename={backup}",
+    ]
+    subprocess.run(cmd)
+    try_to_remove(write)
+    try_to_remove(backup)
+    are_identical = filecmp.cmp(txt_path, expected, shallow=False)
+    assert are_identical
+    txt_path.unlink()
+
+
+def test_cli_remove_before():
+    read = test_data / "SCALAR_STRING_test_data.pb"
+    write = results / "SCALAR_STRING_reduce_by_factor.pb"
+    backup = results / "tmp.pb"
+    expected = cli_output / "SCALAR_STRING_remove_before.pb"
+    ts = ["1", "1", "0", "1", "5"]
+    cmd = [
+        "aa-remove-data-before",
+        read,
+        f"--new-filename={write}",
+        f"--backup-filename={backup}",
+        "--ts",
+    ] + ts
+    subprocess.run(cmd)
+    try_to_remove(backup)
+    are_identical = filecmp.cmp(write, expected, shallow=False)
+    assert are_identical
+    write.unlink()
+
+
+def test_cli_remove_before_backup():
+    read = test_data / "SCALAR_STRING_test_data.pb"
+    write = results / "tmp.pb"
+    backup = results / "SCALAR_STRING_remove_before_backup.pb"
+    expected = read
+    ts = ["1", "1", "0", "1", "5"]
+    cmd = [
+        "aa-remove-data-before",
+        read,
+        f"--new-filename={write}",
+        f"--backup-filename={backup}",
+        "--ts",
+    ] + ts
+    subprocess.run(cmd)
+    try_to_remove(write)
+    are_identical = filecmp.cmp(backup, expected, shallow=False)
+    assert are_identical
+    backup.unlink()
+
+
+def test_cli_remove_before_txt():
+    read = test_data / "SCALAR_STRING_test_data.pb"
+    write = results / "SCALAR_STRING_remove_before_txt.pb"
+    backup = results / "tmp.pb"
+    expected = cli_output / "SCALAR_STRING_remove_before.txt"
+    txt_path = write.with_suffix(".txt")
+    ts = ["1", "1", "0", "1", "5"]
+    cmd = [
+        "aa-remove-data-before",
+        read,
+        "-t",
+        f"--new-filename={write}",
+        f"--backup-filename={backup}",
+        "--ts",
+    ] + ts
+    subprocess.run(cmd)
+    try_to_remove(write)
+    try_to_remove(backup)
+    are_identical = filecmp.cmp(txt_path, expected, shallow=False)
+    assert are_identical
+    txt_path.unlink()
+
+
+def test_cli_remove_after():
+    read = test_data / "SCALAR_STRING_test_data.pb"
+    write = results / "SCALAR_STRING_reduce_by_factor.pb"
+    backup = results / "tmp.pb"
+    expected = cli_output / "SCALAR_STRING_remove_after.pb"
+    ts = ["1", "1", "0", "1", "5"]
+    cmd = [
+        "aa-remove-data-after",
+        read,
+        f"--new-filename={write}",
+        f"--backup-filename={backup}",
+        "--ts",
+    ] + ts
+    subprocess.run(cmd)
+    try_to_remove(backup)
+    are_identical = filecmp.cmp(write, expected, shallow=False)
+    assert are_identical
+    write.unlink()
+
+
+def test_cli_remove_after_backup():
+    read = test_data / "SCALAR_STRING_test_data.pb"
+    write = results / "tmp.pb"
+    backup = results / "SCALAR_STRING_remove_after_backup.pb"
+    expected = read
+    ts = ["1", "1", "0", "1", "5"]
+    cmd = [
+        "aa-remove-data-after",
+        read,
+        f"--new-filename={write}",
+        f"--backup-filename={backup}",
+        "--ts",
+    ] + ts
+    subprocess.run(cmd)
+    try_to_remove(write)
+    are_identical = filecmp.cmp(backup, expected, shallow=False)
+    assert are_identical
+    backup.unlink()
+
+
+def test_cli_remove_after_txt():
+    read = test_data / "SCALAR_STRING_test_data.pb"
+    write = results / "SCALAR_STRING_remove_after_txt.pb"
+    backup = results / "tmp.pb"
+    expected = cli_output / "SCALAR_STRING_remove_after.txt"
+    txt_path = write.with_suffix(".txt")
+    ts = ["1", "1", "0", "1", "5"]
+    cmd = [
+        "aa-remove-data-after",
+        read,
+        "-t",
+        f"--new-filename={write}",
+        f"--backup-filename={backup}",
+        "--ts",
+    ] + ts
+    subprocess.run(cmd)
+    try_to_remove(write)
+    try_to_remove(backup)
+    are_identical = filecmp.cmp(txt_path, expected, shallow=False)
+    assert are_identical
+    txt_path.unlink()
+
+
+def try_to_remove(filepath: PathLike):
+    filepath = Path(filepath)
+    if filepath.is_file():
+        filepath.unlink()
