543a3586f3d9cd9338925fbe4a9aeffe5b8ba337
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2025-01-23 13:54:34 +0000

    Remove duplicate lines

--- src/aa_remove_data/remove_data.py
+++ src/aa_remove_data/remove_data.py
@@ -348,7 +348,6 @@ def aa_remove_data_before():
     if len(timestamp) == 1:
         timestamp.append(1)
 
-    txt_filepath = new_pb.with_suffix(".txt")
     diff = datetime(*([year] + timestamp)) - datetime(year, 1, 1)
     seconds = int(diff.total_seconds())
     txt_filepath = new_pb.with_suffix(".txt")
@@ -399,7 +398,6 @@ def aa_remove_data_after():
     if len(timestamp) == 1:
         timestamp.append(1)
 
-    txt_filepath = new_pb.with_suffix(".txt")
     diff = datetime(*([year] + timestamp)) - datetime(year, 1, 1)
     seconds = int(diff.total_seconds())
     txt_filepath = new_pb.with_suffix(".txt")
